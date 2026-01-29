58415f0623b1ecb36d0635c3da29c94d91947d8f
JPclass105 <fabcl@naver.com>
2026-01-29 14:15:57 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,19 @@
+// 현재 페이지 탭 강조
+document.addEventListener("DOMContentLoaded", function () {
+    const path = window.location.pathname.split("/").pop();
+    const map = {
+        "index.html": "homeTab",
+        "page1.html": "tab1",
+        "page2.html": "tab2",
+        "page3.html": "tab3",
+        "page4.html": "tab4",
+        "page6.html": "tab6",
+        "page7.html": "tab7",
+        "page8.html": "tab8",
+        "page9.html": "tab9"
+    };
+    const activeId = map[path];
+    if (activeId) {
+        document.getElementById(activeId).style.outline = "3px solid #333";
+    }
+});
